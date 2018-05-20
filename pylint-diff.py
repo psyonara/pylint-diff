cd6d3aa2a0e4b10146c79b3ca798e9eccc120904
Helmut Irle <helmut@devigninnovation.com>
2018-05-20 15:21:50 +0200

    Converted to main function.

--- pylint-diff.py
+++ pylint-diff.py
@@ -1,17 +1,27 @@
 import os
 
-from git_functions import get_file_contents_from_branch
+from git_functions import get_file_contents_from_branch, get_current_branch_name
 from pylint_functions import get_pylint_score
 
 
-output = get_file_contents_from_branch("contacts/queries.py", "master")
-with open('contacts_queries.py', mode='w') as fh:
-    fh.write(output)
+def main():
+    branch = get_current_branch_name()
+    print(branch)
+    if branch == 'master':
+        print("Cannot run on master.")
 
-score1 = get_pylint_score('contacts_queries.py')
-os.remove('contacts_queries.py')
-score2 = get_pylint_score('contacts/queries.py')
+    output = get_file_contents_from_branch("contacts/queries.py", "master")
+    with open('contacts_queries.py', mode='w') as fh:
+        fh.write(output)
 
-print('Master score: %s' % score1)
-print('Modified score: %s' % score2)
-print('Score diff: %s' % (score2 - score1))
+    score1 = get_pylint_score('contacts_queries.py')
+    os.remove('contacts_queries.py')
+    score2 = get_pylint_score('contacts/queries.py')
+
+    print('Master score: %s' % score1)
+    print('Modified score: %s' % score2)
+    print('Score diff: %s' % (score2 - score1))
+
+
+if __name__ == "__main__":
+    main()
