174d47aa25cd4756f077ede119a24e5f109525b1
Helmut Irle <helmut@devigninnovation.com>
2018-05-20 15:21:04 +0200

    Added function to get branch name, function to get changes on branch.

--- git_functions.py
+++ git_functions.py
@@ -10,3 +10,23 @@ def get_file_contents_from_branch(filename, branch_name):
     """
     proc = subprocess.Popen(['git', 'show', '%s:%s' % (branch_name, filename)], stdout=subprocess.PIPE)
     return proc.stdout.read().decode()
+
+
+def get_current_branch_name():
+    """
+    Gets the name of the current git branch in the working directory.
+    :return: Name of the branch
+    """
+    proc = subprocess.Popen(['git', 'rev-parse', '--abbrev-ref', 'HEAD'], stdout=subprocess.PIPE)
+    return proc.stdout.read().decode()
+
+
+def get_changed_files(branch1, branch2):
+    """
+    Gets a list of changed files between two branches.
+    :param branch1: name of first branch
+    :param branch2: name of second branch
+    :return: A list of changed files
+    """
+    proc = subprocess.Popen(['git', 'diff', '--name-only', branch1, branch2], stdout=subprocess.PIPE)
+    return proc.stdout.read().decode()
