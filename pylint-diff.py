8aae69f1d9969838ce3034cc0b79cca7acff31df
Helmut Irle <helmut@devigninnovation.com>
2018-10-05 22:08:16 +0200

    Switched to file_name arg.

--- pylint-diff.py
+++ pylint-diff.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 
 from git_functions import get_file_contents_from_branch, get_current_branch_name
@@ -5,18 +6,29 @@ from pylint_functions import get_pylint_score
 
 
 def main():
+    parser = argparse.ArgumentParser(
+        description='Calculate the pylint score difference between two branches for a particular file.'
+    )
+    parser.add_argument(
+        "file_name",
+        help="The path to the file name that should be compared across two branches."
+    )
+    args = parser.parse_args()
     branch = get_current_branch_name()
     print(branch)
     if branch == "master":
         print("Cannot run on master.")
 
-    output = get_file_contents_from_branch("contacts/queries.py", "master")
-    with open("contacts_queries.py", mode="w") as fh:
+    if not os.path.isdir("temp"):
+        os.makedirs("temp")
+
+    output = get_file_contents_from_branch(args.file_name, "master")
+    with open(f"temp/{args.file_name}", mode="w") as fh:
         fh.write(output)
 
-    score1 = get_pylint_score("contacts_queries.py")
-    os.remove("contacts_queries.py")
-    score2 = get_pylint_score("contacts/queries.py")
+    score1 = get_pylint_score(args.file_name)
+    os.remove(f"temp/{args.file_name}")
+    score2 = get_pylint_score(args.file_name)
 
     print("Master score: %s" % score1)
     print("Modified score: %s" % score2)
